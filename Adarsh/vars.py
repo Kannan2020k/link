0e97fc9938e0404dcf9445fa672079841dc34e3c
LieStage <118051769+LieStage@users.noreply.github.com>
2023-04-26 21:51:04 +0530

    Update vars.py

--- Adarsh/vars.py
+++ Adarsh/vars.py
@@ -38,3 +38,5 @@ class Var(object):
     DATABASE_URL = str(getenv('DATABASE_URL'))
     UPDATES_CHANNEL = str(getenv('UPDATES_CHANNEL', None))
     BANNED_CHANNELS = list(set(int(x) for x in str(getenv("BANNED_CHANNELS", "-1001362659779")).split())) 
+SHORTENER_API = str(getenv("SHORTENER_API", None))
+LONG_DROPLINK_URL = str(get("LONG_DROPLINK_URL", False))
