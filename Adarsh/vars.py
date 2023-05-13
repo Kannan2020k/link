c83f7b78c9747a7f0f8ad21d36dbdf7f6d5040ee
LieStage <118051769+LieStage@users.noreply.github.com>
2023-05-13 09:12:05 +0530

    Update vars.py

--- Adarsh/vars.py
+++ Adarsh/vars.py
@@ -7,36 +7,65 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+
 class Var(object):
+
     MULTI_CLIENT = False
-    API_ID = int(getenv('API_ID'))
-    API_HASH = str(getenv('API_HASH'))
-    BOT_TOKEN = str(getenv('BOT_TOKEN'))
-    name = str(getenv('name', 'filetolinkbot'))
+
+    API_ID = int(getenv('API_ID',"4682685"))
+
+    API_HASH = str(getenv('API_HASH',"3eba5d471162181b8a3f7f5c0a23c307"))
+
+    BOT_TOKEN = str(getenv('BOT_TOKEN',"5544919313:AAGInu1jJdrdiNRjDNh2hqk1akzPNu5rfiI"))
+
+    name = str(getenv('SESSION_NAME', 'filetolinkbot'))
+
     SLEEP_THRESHOLD = int(getenv('SLEEP_THRESHOLD', '60'))
+
     WORKERS = int(getenv('WORKERS', '4'))
-    BIN_CHANNEL = int(getenv('BIN_CHANNEL'))
-    PORT = int(getenv('PORT', 8080))
+
+    BIN_CHANNEL = int(getenv('BIN_CHANNEL','-1001539366814'))
+
+    PORT = int(getenv('PORT', 8000))
+
     BIND_ADRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', '0.0.0.0'))
+
     PING_INTERVAL = int(environ.get("PING_INTERVAL", "1200"))  # 20 minutes
-    OWNER_ID = set(int(x) for x in os.environ.get("OWNER_ID", "").split())  
+
+    OWNER_ID = set(int(x) for x in os.environ.get("OWNER_ID", "945284066").split())  
+
     NO_PORT = bool(getenv('NO_PORT', False))
-    SHORTENER_API = str(getenv("SHORTENER_API","c1c6e39e62ec0186c6be3b72c3cddc272177966a"))
-    LONG_DROPLINK_URL = str(get("LONG_DROPLINK_URL","https://shorturllink.in/api"))
-    APP_NAME = None
-    OWNER_USERNAME = str(getenv('OWNER_USERNAME'))
+
+    APP_NAME = str(getenv('APP_NAME','unfortunate-malissa-aakash2021-cap.koyeb.app')) #@fligher
+
+    OWNER_USERNAME = str(getenv('OWNER_USERNAME',"FLIGHER"))
+
     if 'DYNO' in environ:
+
         ON_HEROKU = True
-        APP_NAME = str(getenv('APP_NAME'))
-    
+
+        APP_NAME = str(getenv('APP_NAME',"filetolinktb")) #
+
     else:
-        ON_HEROKU = False
-    FQDN = str(getenv('FQDN', BIND_ADRESS)) if not ON_HEROKU or getenv('FQDN') else APP_NAME+'.herokuapp.com'
+
+    ON_HEROKU = False 
+
+    FQDN = str(getenv('FQDN',BIND_ADRESS)) if not ON_HEROKU or getenv(FQDN) else APP_NAME + "render.com"
+
+    FQDN = str(getenv('FQDN',APP_NAME))
+
     HAS_SSL=bool(getenv('HAS_SSL',False))
+
     if HAS_SSL:
+
         URL = "https://{}/".format(FQDN)
+
     else:
+
         URL = "http://{}/".format(FQDN)
-    DATABASE_URL = str(getenv('DATABASE_URL'))
-    UPDATES_CHANNEL = str(getenv('UPDATES_CHANNEL', None))
-    BANNED_CHANNELS = list(set(int(x) for x in str(getenv("BANNED_CHANNELS", "-1001362659779")).split())) 
+
+    DATABASE_URL = str(getenv('DATABASE_URL',"mongodb+srv://twobot:cbot22@cluster0.tbf5o.mongodb.net/?retryWrites=true&w=majority"))
+
+    UPDATES_CHANNEL = str(getenv('UPDATES_CHANNEL', "redirect_to_lion_stage"))
+
+    BANNED_CHANNELS = list(set(int(x) for x in str(getenv("BANNED_CHANNELS", "-1001362659779")).split()))
