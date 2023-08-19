59ce671b78849a5cd667583bf1d2655b2f0632df
LieStage <118051769+LieStage@users.noreply.github.com>
2023-08-19 20:08:16 +0530

    Update shortner.py

--- Adarsh/utils/shortner.py
+++ Adarsh/utils/shortner.py
@@ -1,3 +1,8 @@
+import re
+import os
+import asyncio
+from info import LONG_DROPLINK_URL, SHORTENER_API
+shortz = shortzy.Shortzy(SHORTENER_API, "dtglinks.in")
 async def get_shortlink(link):
     if SHORTENER_API:
         if LONG_DROPLINK_URL == "True" or LONG_DROPLINK_URL is True:
